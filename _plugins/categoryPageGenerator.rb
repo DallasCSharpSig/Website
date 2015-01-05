e0b1daa547952c108175f4bfe1a4e63aaa68e64a
Michael Dudley <michael.dudley@improvingenterprises.com>
2015-01-04 20:28:14 -0600

    creating a plugin for meetings

---
+++ _plugins/categoryPageGenerator.rb
@@ -0,0 +1,40 @@
+module Jekyll
+
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+
+      meetings = site.collections['meetings'].docs.map {
+      	|doc| doc.data
+      }
+
+      groupedMeetings = meetings.group_by { 
+      	|meeting| Date.strptime(meeting['date']).year 
+      }
+
+      self.data['groupedMeetings'] = groupedMeetings
+      puts self.data
+
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        dir = site.config['category_dir'] || 'categories'
+        site.categories.each_key do |category|
+          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+        end
+      end
+    end
+  end
+
+end
